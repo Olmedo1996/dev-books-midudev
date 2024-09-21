89507cd7a9f47273192440f499263c66aa18f260
diego <diegoolmedog96@gmail.com>
2024-09-21 16:23:03 -0400

    feat: add books store

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [tailwind()],
+  env: {
+    schema: {
+      SHOW_BY_BUTTON: envField.boolean({ context:'server', access:'public'}),
+      SCORE_API_BUTTON: envField.string({ context:'server', access:'public'})
+    }
+  }
+});
